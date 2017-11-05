2d95af34c358b8a4cb474e8d9d7c11c11c2b5211
Stuart Mahon <stuart.mahon@alaress.com.au>
2017-11-05 14:09:23 +1100

    Part 2

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,13 +1,17 @@
-let blankLine = '  |   |  ';
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
+const printBoard = board => {
+    console.log('Current Board:');
+    console.log(board[0].join(' | '));
+    console.log(board[1].join(' | '));
+    console.log(board[2].join(' | '));
+};
 
-console.log(`This is what an empty board would look like:`);
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+let board = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+];
 
-console.log(`This is what a board with a guess and a bomb on it would look like:`);
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+printBoard(board);
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
